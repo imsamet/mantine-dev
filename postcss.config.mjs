382b06a6ea52ea01cf74528f78096434c67fc434
Samet Çetin <46278098+imsamet@users.noreply.github.com>
2024-08-07 11:24:50 +0300

    complated project configrations

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,6 +2,17 @@
 const config = {
   plugins: {
     tailwindcss: {},
+    'postcss-preset-mantine': {},
+    'postcss-simple-vars': {
+      variables: {
+        'mantine-breakpoint-xs': '29.6875rem',
+        'mantine-breakpoint-sm': '40rem',
+        'mantine-breakpoint-md': '48rem',
+        'mantine-breakpoint-lg': '64rem',
+        'mantine-breakpoint-xl': '80rem',
+        'mantine-breakpoint-2xl': '96rem',
+      },
+    },
   },
 };
 
